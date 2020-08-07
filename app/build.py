1884eced80296502809d4beb60b7cbdb01d67339
someshsingh22 <someshsingh9414@gmail.com>
2020-08-07 22:53:56 +0530

    Formatting

--- app/build.py
+++ app/build.py
@@ -1,51 +1,65 @@
-#Basic
+# Basic
 import time
 import pandas as pd
 import pickle
-from scipy import sparse #Sparsed Vectorizers
-import scipy.stats as ss 
+from scipy import sparse  # Sparsed Vectorizers
+import scipy.stats as ss
 from scipy.sparse import hstack
 
-#Vectorizers
+# Vectorizers
 from sklearn.feature_extraction.text import CountVectorizer as CV
 from sklearn.feature_extraction.text import TfidfVectorizer as TF
 
-#MNB Classifier Modelling
+# MNB Classifier Modelling
 from sklearn.naive_bayes import MultinomialNB as MNB
 from sklearn.utils import resample
 import numpy as np
+
 
 def sampler(df, thresh=22700):
     """
     Undersamples/Upsamples labels to a given threshold
     """
-    f_df={k:df[df['Flair']==k] for k in set(df['Flair'])}
-    train_df=pd.concat([resample(data[100:], n_samples=22700, replace=True, random_state=123) for data in f_df.values()])
+    f_df = {k: df[df["Flair"] == k] for k in set(df["Flair"])}
+    train_df = pd.concat(
+        [
+            resample(data[100:], n_samples=22700, replace=True, random_state=123)
+            for data in f_df.values()
+        ]
+    )
     return train_df
+
 
 if __name__ == "__main__":
 
-    #Target Paths
-    VEC_PATH = 'tfidf.pickle'
-    MODEL_PATH = 'model.pickle'
+    # Target Paths
+    VEC_PATH = "tfidf.pickle"
+    MODEL_PATH = "model.pickle"
 
-    #Load Data
-    DATA_PATH = 'data.csv'
-    df=pd.read_csv(open(DATA_PATH), encoding='utf-8').dropna()
+    # Load Data
+    DATA_PATH = "data.csv"
+    df = pd.read_csv(open(DATA_PATH), encoding="utf-8").dropna()
 
-    #UNDERSAMPLE
+    # UNDERSAMPLE
     train_df = sampler(df)
-    
-    #Feature Extractor
-    cv=TF(sublinear_tf=True,min_df=10, encoding='latin-1',ngram_range=(1,1),stop_words='english', analyzer='word', max_features=6150)
-    cv.fit(df['Title']+' '+df['Text'])
-    pickle.dump(model, open(VEC_PATH, 'wb'))
 
-    #Build Model
-    model= MNB()
-    train_data=hstack((cv.transform(train_df['Title']),cv.transform(train_df['Text'])))
-    model.fit(train_data, train_df['Flair'])
-    pickle.dump(model, open(MODEL_PATH, 'wb'))
+    # Feature Extractor
+    cv = TF(
+        sublinear_tf=True,
+        min_df=10,
+        encoding="latin-1",
+        ngram_range=(1, 1),
+        stop_words="english",
+        analyzer="word",
+        max_features=6150,
+    )
+    cv.fit(df["Title"] + " " + df["Text"])
+    pickle.dump(model, open(VEC_PATH, "wb"))
 
-
-
+    # Build Model
+    model = MNB()
+    train_data = hstack(
+        (cv.transform(train_df["Title"]), cv.transform(train_df["Text"]))
+    )
+    model.fit(train_data, train_df["Flair"])
+    pickle.dump(model, open(MODEL_PATH, "wb"))
--- app/tfidf_model.py
+++ app/tfidf_model.py
@@ -2,57 +2,73 @@ from flask import Flask, request, render_template
 from werkzeug.datastructures import ImmutableMultiDict
 from sklearn.feature_extraction.text import TfidfVectorizer as TF
 from sklearn.naive_bayes import MultinomialNB as MNB
-from scipy import sparse #Sparsed Vectorizers
+from scipy import sparse  # Sparsed Vectorizers
 from scipy.sparse import hstack
-import scipy.stats as ss 
+import scipy.stats as ss
 import re
 from sklearn.utils import resample
 import numpy as np
 import requests
 import pickle
 
+
 def query(url):
-	id=url[:-1].split('/')[-2]
-	r=requests.get('https://api.pushshift.io/reddit/search/submission/?ids={}&fields=selftext,title'.format(id))
-	data=r.json()['data'][0]
-	return data['selftext'], data['title']
+    id = url[:-1].split("/")[-2]
+    r = requests.get(
+        "https://api.pushshift.io/reddit/search/submission/?ids={}&fields=selftext,title".format(
+            id
+        )
+    )
+    data = r.json()["data"][0]
+    return data["selftext"], data["title"]
+
 
 def multi_query(inp):
-	urls=inp.split()
-	ids=[url[:-1].split('/')[-2] for url in urls]
-	data=[]
-	for id in ids:
-		r=requests.get('https://api.pushshift.io/reddit/search/submission/?ids={}&fields=selftext,title'.format(id))
-		dat=r.json()['data'][0]
-		data.append(dat['title']+' '+dat['selftext'])
-	return urls,data
+    urls = inp.split()
+    ids = [url[:-1].split("/")[-2] for url in urls]
+    data = []
+    for id in ids:
+        r = requests.get(
+            "https://api.pushshift.io/reddit/search/submission/?ids={}&fields=selftext,title".format(
+                id
+            )
+        )
+        dat = r.json()["data"][0]
+        data.append(dat["title"] + " " + dat["selftext"])
+    return urls, data
+
 
 def clean(text):
-	contracts={
-	"n't" : " not",
-	"'ll" : "will",
-	"'ve" : "have",
-	"'n" : "",
-	"'ed" : "",
-	"'l" : "will",
-	"'re" : "are",
-	"'r" : "are",
-	"'a" : " is a",
-	"'d" : "had",
-	"'ing" : "ing",
-	"r\/" : " subreddit ",
-	"u\/" : " user ",
-	"&amp;" : " ",
-	"&gt;" : " ",
-	"&lt;" : " ",
-	"&gt" : "  ",
-	"&lt" : " ",
-	"#" : "#  ",}
-	text=text.lower()
-	text=text.replace('\n', ' ')
-	for k,v in contracts.items():
-		text=text.replace(k,v)
-	text=re.sub('[(]?http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F])/)*', ' ', text)
-	text=re.sub('[^a-z]', ' ', text)
-	text=' '.join([word for word in text.split(' ') if not word== ''])
-	return text
+    contracts = {
+        "n't": " not",
+        "'ll": "will",
+        "'ve": "have",
+        "'n": "",
+        "'ed": "",
+        "'l": "will",
+        "'re": "are",
+        "'r": "are",
+        "'a": " is a",
+        "'d": "had",
+        "'ing": "ing",
+        "r\/": " subreddit ",
+        "u\/": " user ",
+        "&amp;": " ",
+        "&gt;": " ",
+        "&lt;": " ",
+        "&gt": "  ",
+        "&lt": " ",
+        "#": "#  ",
+    }
+    text = text.lower()
+    text = text.replace("\n", " ")
+    for k, v in contracts.items():
+        text = text.replace(k, v)
+    text = re.sub(
+        "[(]?http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F])/)*",
+        " ",
+        text,
+    )
+    text = re.sub("[^a-z]", " ", text)
+    text = " ".join([word for word in text.split(" ") if not word == ""])
+    return text
